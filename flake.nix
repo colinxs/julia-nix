b42b0a93efe6e52e09ad56d7fa842cefd61a266e
Colin Summers <me@colinxsummers.com>
2021-09-15 04:24:23 -0600

    save

--- flake.nix
+++ flake.nix
@@ -11,8 +11,36 @@
   # };
   outputs = { self, nixpkgs, nix-home, ... }: 
     let
-      # pkgs = import nixpkgs { config.allowBroken = true; };
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      pkgs = import nixpkgs {
+        overlays = [
+          final: prev: {
+            ccacheWrapper = prev.ccacheWrapper.override {
+              extraConfig = ''
+                export CCACHE_COMPRESS=1
+                export CCACHE_DIR=/var/cache/ccache
+                export CCACHE_UMASK=007
+                if [ ! -d "$CCACHE_DIR" ]; then
+                  echo "====="
+                  echo "Directory '$CCACHE_DIR' does not exist"
+                  echo "Please create it with:"
+                  echo "  sudo mkdir -m0770 '$CCACHE_DIR'"
+                  echo "  sudo chown root:nixbld '$CCACHE_DIR'"
+                  echo "====="
+                  exit 1
+                fi
+                if [ ! -w "$CCACHE_DIR" ]; then
+                  echo "====="
+                  echo "Directory '$CCACHE_DIR' is not accessible for user $(whoami)"
+                  echo "Please verify its access permissions"
+                  echo "====="
+                  exit 1
+                fi
+              '';
+            };
+          }
+        ];
+      };
+      # pkgs = nixpkgs.legacyPackages.x86_64-linux;
       pkgsHome = nix-home.legacyPackages.x86_64-linux; 
       args = { inherit (pkgs.darwin.apple_sdk.frameworks) ApplicationServices CoreServices; };
     in {
