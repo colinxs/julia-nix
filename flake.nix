f313ca8a31d5bb209a1f475216c05d2ac9971ca5
Colin Summers <me@colinxsummers.com>
2021-09-14 21:20:23 -0600

    save

--- flake.nix
+++ flake.nix
@@ -14,8 +14,6 @@
       # pkgs = import nixpkgs { config.allowBroken = true; };
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
       pkgsHome = nix-home.legacyPackages.x86_64-linux; 
-      julia = (pkgs.callPackage ./NixManifest.nix { inherit pkgs; }).julia;
-      src = julia.meta.assets."julia-${julia.version}-full.tar.gz";
       args = { inherit src; inherit (pkgs.darwin.apple_sdk.frameworks) ApplicationServices CoreServices; };
     in {
       packages.x86_64-linux.julia = pkgs.callPackage ./default.nix args;
