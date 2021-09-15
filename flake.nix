8f43d2e41768b63fbc7062ccd9ee29ddf624eb25
Colin Summers <me@colinxsummers.com>
2021-09-15 04:37:05 -0600

    save

--- flake.nix
+++ flake.nix
@@ -16,30 +16,31 @@
         inherit system;
         overlays = [
           (final: prev: {
-            replaceStdenv = { pkgs }: builtins.trace "HERE" pkgs.ccacheStdenv;
-            # stdenv = prev.ccacheStdenv;
-            ccacheWrapper = prev.ccacheWrapper.override {
-              extraConfig = ''
-                export CCACHE_COMPRESS=1
-                export CCACHE_DIR=/var/cache/ccache
-                export CCACHE_UMASK=007
-                # if [ ! -d "$CCACHE_DIR" ]; then
-                #   echo "====="
-                #   echo "Directory '$CCACHE_DIR' does not exist"
-                #   echo "Please create it with:"
-                #   echo "  sudo mkdir -m0770 '$CCACHE_DIR'"
-                #   echo "  sudo chown root:nixbld '$CCACHE_DIR'"
-                #   echo "====="
-                #   exit 1
-                # fi
-                # if [ ! -w "$CCACHE_DIR" ]; then
-                #   echo "====="
-                #   echo "Directory '$CCACHE_DIR' is not accessible for user $(whoami)"
-                #   echo "Please verify its access permissions"
-                #   echo "====="
-                #   exit 1
-                # fi
-              '';
+            config = {
+              replaceStdenv = { pkgs }: builtins.trace "HERE" pkgs.ccacheStdenv;
+              ccacheWrapper = prev.ccacheWrapper.override {
+                extraConfig = ''
+                  export CCACHE_COMPRESS=1
+                  export CCACHE_DIR=/var/cache/ccache
+                  export CCACHE_UMASK=007
+                  # if [ ! -d "$CCACHE_DIR" ]; then
+                  #   echo "====="
+                  #   echo "Directory '$CCACHE_DIR' does not exist"
+                  #   echo "Please create it with:"
+                  #   echo "  sudo mkdir -m0770 '$CCACHE_DIR'"
+                  #   echo "  sudo chown root:nixbld '$CCACHE_DIR'"
+                  #   echo "====="
+                  #   exit 1
+                  # fi
+                  # if [ ! -w "$CCACHE_DIR" ]; then
+                  #   echo "====="
+                  #   echo "Directory '$CCACHE_DIR' is not accessible for user $(whoami)"
+                  #   echo "Please verify its access permissions"
+                  #   echo "====="
+                  #   exit 1
+                  # fi
+                '';
+              };
             };
           })
         ];
