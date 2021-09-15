e1fcb1cce849acc3b89234ae7293785b47a2695a
Colin Summers <me@colinxsummers.com>
2021-09-15 05:32:21 -0600

    save

--- flake.nix
+++ flake.nix
@@ -37,7 +37,7 @@
               extraConfig = ''
                 export CCACHE_COMPRESS=1
                 export CCACHE_DIR=/var/cache/ccache
-             /nix/store/jmx4m5naalqywdnkr7az73wv3yr3ld7m-ccache-links/bin/gcc   export CCACHE_UMASK=007
+                export CCACHE_UMASK=007
                 if [ ! -d "$CCACHE_DIR" ]; then
                   echo "====="
                   echo "Directory '$CCACHE_DIR' does not exist"
