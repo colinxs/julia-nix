2d9adc5300cfb535f1c0271affbf939be1e6d5f9
Colin Summers <me@colinxsummers.com>
2021-09-15 06:02:02 -0600

    save

--- flake.nix
+++ flake.nix
@@ -19,55 +19,56 @@
       #     profiledCompiler = true; 
       #   };
       # });
+      stdenv = with pkgs; overrideCC pkgs.stdenv (ccacheWrapper.override { inherit cc; }); 
       pkgs = import nixpkgs {
         inherit system;
         # config = {
         #   replaceStdenv = { pkgs }: pkgs.ccacheStdenv;
         # };
-        overlays = [
-          (final: prev: {
-            ccacheWrapper = prev.ccacheWrapper.override {
-              cc = cc;
-              # cc = final.buildPackages.gcc8;
-              # cc = prev.gcc9Stdenv.cc;
-              # cc = prev.fastStdenv.cc;
-              # cc = prev.buildPackages.gcc10.overrideAttrs (oA: {
-              #   cc = oA.cc.override {
-              #     reproducibleBuild = false;
-              #     profiledCompiler = true; 
-              #   };
-              # });
-
-              # cc = prev.buildPackages.gcc10.overrideAttrs (old: {
-              #   cc = old.cc.override {
-              #     reproducibleBuild = false;
-              #     profiledCompiler = with stdenv; (!isDarwin && (isi686 || isx86_64));
-              #   };
-              # });
-              extraConfig = ''
-                export CCACHE_COMPRESS=1
-                export CCACHE_DIR=/var/cache/ccache
-                export CCACHE_UMASK=007
-                if [ ! -d "$CCACHE_DIR" ]; then
-                  echo "====="
-                  echo "Directory '$CCACHE_DIR' does not exist"
-                  echo "Please create it with:"
-                  echo "  sudo mkdir -m0770 '$CCACHE_DIR'"
-                  echo "  sudo chown root:nixbld '$CCACHE_DIR'"
-                  echo "====="
-                  exit 1
-                fi
-                if [ ! -w "$CCACHE_DIR" ]; then
-                  echo "====="
-                  echo "Directory '$CCACHE_DIR' is not accessible for user $(whoami)"
-                  echo "Please verify its access permissions"
-                  echo "====="
-                  exit 1
-                fi
-              '';
-            };
-          })
-        ];
+        # overlays = [
+        #   (final: prev: {
+        #     ccacheWrapper = prev.ccacheWrapper.override {
+        #       cc = cc;
+        #       # cc = final.buildPackages.gcc8;
+        #       # cc = prev.gcc9Stdenv.cc;
+        #       # cc = prev.fastStdenv.cc;
+        #       # cc = prev.buildPackages.gcc10.overrideAttrs (oA: {
+        #       #   cc = oA.cc.override {
+        #       #     reproducibleBuild = false;
+        #       #     profiledCompiler = true; 
+        #       #   };
+        #       # });
+        #
+        #       # cc = prev.buildPackages.gcc10.overrideAttrs (old: {
+        #       #   cc = old.cc.override {
+        #       #     reproducibleBuild = false;
+        #       #     profiledCompiler = with stdenv; (!isDarwin && (isi686 || isx86_64));
+        #       #   };
+        #       # });
+        #       extraConfig = ''
+        #         export CCACHE_COMPRESS=1
+        #         export CCACHE_DIR=/var/cache/ccache
+        #         export CCACHE_UMASK=007
+        #         if [ ! -d "$CCACHE_DIR" ]; then
+        #           echo "====="
+        #           echo "Directory '$CCACHE_DIR' does not exist"
+        #           echo "Please create it with:"
+        #           echo "  sudo mkdir -m0770 '$CCACHE_DIR'"
+        #           echo "  sudo chown root:nixbld '$CCACHE_DIR'"
+        #           echo "====="
+        #           exit 1
+        #         fi
+        #         if [ ! -w "$CCACHE_DIR" ]; then
+        #           echo "====="
+        #           echo "Directory '$CCACHE_DIR' is not accessible for user $(whoami)"
+        #           echo "Please verify its access permissions"
+        #           echo "====="
+        #           exit 1
+        #         fi
+        #       '';
+        #     };
+        #   })
+        # ];
       };
       callPackage = pkgs.lib.callPackageWith (pkgs // { stdenv = pkgs.ccacheStdenv; });
       stdenv = pkgs.ccacheStdenv;
