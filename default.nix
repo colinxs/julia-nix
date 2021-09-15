d3b9676822f4e48c601e26386f7b46679dbb435d
Colin Summers <me@colinxsummers.com>
2021-09-15 02:24:15 -0600

    save

--- default.nix
+++ default.nix
@@ -325,7 +325,7 @@ stdenv.mkDerivation rec {
 
   preBuild = ''
     makeFlagsArray+=(
-      "JOBS=$NIX_BUILD_CORES"
+      # "JOBS=$NIX_BUILD_CORES"
       # "MAKE_NB_JOBS=$NIX_BUILD_CORES"
     )
     # export MAKEFLAGS="-j $NIX_BUILD_CORES"
