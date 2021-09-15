f5ea9fbb84befcbfb26dfd17d1c4003a7db07c93
Colin Summers <me@colinxsummers.com>
2021-09-14 19:12:23 -0600

    save

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
   outputs = { self, nixpkgs, src }: 
     let
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
-      args = { inherit src; inherit (darwin.apple_sdk.frameworks) ApplicationServices CoreServices; };
+      args = { inherit src; inherit (pkgs.darwin.apple_sdk.frameworks) ApplicationServices CoreServices; };
     in {
       # packages.x86_64-linux.julia = pkgs.callPackage ./default.nix args;
       packages.x86_64-linux.julia = pkgs.callPackage ./default-simple.nix args; 
