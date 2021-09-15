f23df9c2c2bfe0c350eef09cb808f813853f9298
Colin Summers <me@colinxsummers.com>
2021-09-15 06:33:09 -0600

    save

--- flake.nix
+++ flake.nix
@@ -75,7 +75,7 @@
         { inherit (pkgs.stdenv) cc; }
       );
 
-      stdenv = pkgs.overrideCC pkgs.stdenv cc; 
+      stdenv = pkgs.overrideCC pkgs.stdenv (ccacheWrapper.override { inherit cc; });
         # cc = pkgs.fastStdenv.cc;
       # stdenv = pkgs.overrideCC pkgs.stdenv (pkgs.ccache.links {
       #   extraConfig = '' 
