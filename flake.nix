baa7cca84169437e49281965507fc5d7b2a1d607
Colin Summers <me@colinxsummers.com>
2021-09-15 06:12:07 -0600

    save

--- flake.nix
+++ flake.nix
@@ -66,7 +66,7 @@
       pkgsHome = nix-home.legacyPackages.x86_64-linux;
       # stdenv = pkgs.fastStdenv;
       # stdenv = pkgs.overrideCC pkgs.ccacheStdenv pkgs.fastStdenv.cc;
-      stdenv = ccacheStdenv;
+      stdenv = pkgs.ccacheStdenv;
       args = {
         inherit (pkgs.darwin.apple_sdk.frameworks) ApplicationServices CoreServices;
         # stdenv = pkgs.ccacheStdenv;
