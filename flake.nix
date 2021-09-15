e0b10080390f86d8b4a88ec9d058e38614edf0b5
Colin Summers <me@colinxsummers.com>
2021-09-15 05:53:45 -0600

    save

--- flake.nix
+++ flake.nix
@@ -63,7 +63,7 @@
       };
       callPackage = pkgs.lib.callPackageWith (pkgs // { stdenv = pkgs.ccacheStdenv; });
       # stdenv = pkgs.ccacheStdenv;
-      cc = buildPackages.gcc9.overrideAttrs (oA: {
+      cc = pkgs.buildPackages.gcc9.overrideAttrs (oA: {
         cc =  oA.cc.override {
           reproducibleBuild = false;
           profiledCompiler = true; 
