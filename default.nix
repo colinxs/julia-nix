587b9b6a3cc1c6335a1fd3a4377cfa8dc5caf367
Colin Summers <me@colinxsummers.com>
2021-09-15 02:28:51 -0600

    save

--- default.nix
+++ default.nix
@@ -231,7 +231,7 @@ stdenv.mkDerivation rec {
     for f in ./deps/**/Makefile ./deps/**/*.mk; do
       # if [ -f "$f" ]; then
         echo "Modifying $f"
-        sed -ri 's/\s+\--jobs=$(JOBS)//g' "$f" || echo "===== FAILED: $f"
+        sed -ri 's/\s+\--jobs=\$\(JOBS\)//g' "$f" || echo "===== FAILED: $f"
         sed -ri 's/\s+\--jobs\s*=\s*[0-9]+//g' "$f" || echo "===== FAILED: $f"
         sed -ri 's/\s+\-j\s*[0-9]+//g' "$f" || echo "===== FAILED: $f"
       # fi
