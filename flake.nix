57499f5b7c739b37deca1abe5ef942a089cb052b
Colin Summers <me@colinxsummers.com>
2021-09-15 05:57:04 -0600

    save

--- flake.nix
+++ flake.nix
@@ -64,7 +64,7 @@
       callPackage = pkgs.lib.callPackageWith (pkgs // { stdenv = pkgs.ccacheStdenv; });
       # stdenv = pkgs.ccacheStdenv;
       # cc = pkgs.gcc9;
-      cc = builtins.trace nixpkgs (pkgs.gcc10.overrideAttrs (oA: {
+      cc = builtins.trace "${nixpkgs}" (pkgs.gcc10.overrideAttrs (oA: {
         cc =  oA.cc.override {
           reproducibleBuild = false;
           profiledCompiler = true; 
