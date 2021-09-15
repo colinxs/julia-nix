44e3b6dcac62be95c1ae7c007b512b2750295afe
Colin Summers <me@colinxsummers.com>
2021-09-14 20:33:44 -0600

    save

--- flake.nix
+++ flake.nix
@@ -11,7 +11,8 @@
   };
   outputs = { self, nixpkgs, nix-home, src, ... }: 
     let
-      pkgs = import nixpkgs { config.allowBroken = true; };
+      # pkgs = import nixpkgs { config.allowBroken = true; };
+      pkgs = pkgs.legacyPackages.x86_64-linux;
       pkgsHome = nix-home.legacyPackages.x86_64-linux; 
       args = { inherit src pkgsHome; inherit (pkgs.darwin.apple_sdk.frameworks) ApplicationServices CoreServices; };
     in {
