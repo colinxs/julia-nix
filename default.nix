c56212f14143fcba0c3c5fb7e265eda15fb8be20
Colin Summers <me@colinxsummers.com>
2021-09-15 03:12:15 -0600

    save

--- default.nix
+++ default.nix
@@ -320,7 +320,7 @@ stdenv.mkDerivation rec {
      
       # TODO
       "USE_BINARYBUILDER=1"
-      "VERBOSE=1"
+      # "VERBOSE=1"
       # "JOBS=$NIX_BUILD_CORES"
       # "MAKE_NB_JOBS=$NIX_BUILD_CORES"
       # "-j$NIX_BUILD_CORES"
